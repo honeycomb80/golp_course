5ff12385dd54c18bcbea646051f3553cd4494942
honeycomb80 <honeycomb8080@gmail.com>
2014-06-03 14:15:06 -0700

    created scaffold, raked db:migrate, added Thing link to nav menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
 
+  resources :things
+
 get "/helloworld", to: "pages#helloworld", as: "helloworld"
 
 end
+
+#     Prefix Verb   URI Pattern                Controller#Action
+#     things GET    /things(.:format)          things#index
+#            POST   /things(.:format)          things#create
+#  new_thing GET    /things/new(.:format)      things#new
+# edit_thing GET    /things/:id/edit(.:format) things#edit
+#      thing GET    /things/:id(.:format)      things#show
+#            PATCH  /things/:id(.:format)      things#update
+#            PUT    /things/:id(.:format)      things#update
+#            DELETE /things/:id(.:format)      things#destroy
+# helloworld GET    /helloworld(.:format)      pages#helloworld
